d2200e514b99cf58e2d3d59998745112066f8314
Juan Pablo Lausi <juanpablo_lausi@hotmail.com.ar>
2024-05-31 21:10:15 -0300

    Agrego restricciones foraneas, not null y cambio tipos en numero dni

--- sql/init.sql
+++ sql/init.sql
@@ -1,91 +1,91 @@
 
 CREATE TABLE PERSONA (
-    tipoDoc VARCHAR(10),
-    NroDocumento VARCHAR(20),
-    apellido VARCHAR(50),
-    nombre VARCHAR(50),
-    fechaNacimiento DATE,
-    telefono VARCHAR(15),
-    correoElectronico VARCHAR(50),
+    tipoDoc VARCHAR(10) NOT NULL,
+    NroDocumento CHAR(8) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    correoElectronico VARCHAR(50) NOT NULL,
     PRIMARY KEY (tipoDoc, NroDocumento)
 );
 
 CREATE TABLE CLIENTE (
-    tipoDoc VARCHAR(10),
-    NroDocumento VARCHAR(20),
-    situacionFiscal VARCHAR(50),
+    tipoDoc VARCHAR(10) NOT NULL,
+    NroDocumento CHAR(8) NOT NULL,
+    situacionFiscal VARCHAR(50) NOT NULL,
     PRIMARY KEY (tipoDoc, NroDocumento),
-    FOREIGN KEY (tipoDoc, NroDocumento) REFERENCES PERSONA(tipoDoc, NroDocumento)
+    FOREIGN KEY (tipoDoc, NroDocumento) REFERENCES PERSONA(tipoDoc, NroDocumento) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE ENCARGADO (
-    tipoDoc VARCHAR(10),
-    NroDocumento VARCHAR(20),
-    legajo VARCHAR(20),
-    categoria VARCHAR(50),
+    tipoDoc VARCHAR(10) NOT NULL,
+    NroDocumento CHAR(8) NOT NULL,
+    legajo VARCHAR(20) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
     PRIMARY KEY (tipoDoc, NroDocumento),
-    FOREIGN KEY (tipoDoc, NroDocumento) REFERENCES PERSONA(tipoDoc, NroDocumento)
+    FOREIGN KEY (tipoDoc, NroDocumento) REFERENCES PERSONA(tipoDoc, NroDocumento) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE PUBLICIDAD (
-    id INT AUTO_INCREMENT,
-    Duracion INT,
+    id INT AUTO_INCREMENT NOT NULL,
+    Duracion INT NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE SALA (
-    idSala INT AUTO_INCREMENT,
-    nombre VARCHAR(50),
-    cantidadAsientos INT,
-    tipo VARCHAR(50),
-    tipoDocEncargado VARCHAR(10),
-    NroDocumentoEncargado VARCHAR(20),
+    idSala INT AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    cantidadAsientos INT NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    tipoDocEncargado VARCHAR(10) NOT NULL,
+    NroDocumentoEncargado CHAR(8) NOT NULL,
     PRIMARY KEY (idSala),
-    FOREIGN KEY (tipoDocEncargado, NroDocumentoEncargado) REFERENCES ENCARGADO(tipoDoc, NroDocumento)
+    FOREIGN KEY (tipoDocEncargado, NroDocumentoEncargado) REFERENCES ENCARGADO(tipoDoc, NroDocumento) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE ASIENTO (
-    idSala INT,
-    nroAsiento INT,
-    fila VARCHAR(5),
+    idSala INT NOT NULL,
+    nroAsiento INT NOT NULL,
+    fila VARCHAR(5) NOT NULL,
     PRIMARY KEY (idSala, nroAsiento),
-    FOREIGN KEY (idSala) REFERENCES SALA(idSala)
+    FOREIGN KEY (idSala) REFERENCES SALA(idSala) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE FUNCION (
-    IdFuncion INT AUTO_INCREMENT,
-    Fecha DATE,
-    Hora TIME,
-    IdSala INT,
-    idPubicidadInicio INT,
-    idPublicidadFinal INT,
+    IdFuncion INT AUTO_INCREMENT NOT NULL,
+    Fecha DATE NOT NULL,
+    Hora TIME NOT NULL,
+    IdSala INT NOT NULL,
+    idPubicidadInicio INT NOT NULL,
+    idPublicidadFinal INT NOT NULL,
     PRIMARY KEY (IdFuncion),
     FOREIGN KEY (IdSala) REFERENCES SALA(idSala),
-    FOREIGN KEY (idPubicidadInicio) REFERENCES PUBLICIDAD(id),
-    FOREIGN KEY (idPublicidadFinal) REFERENCES PUBLICIDAD(id)
+    FOREIGN KEY (idPubicidadInicio) REFERENCES PUBLICIDAD(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (idPublicidadFinal) REFERENCES PUBLICIDAD(id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE ENTRADA (
-    NumeroEntrada INT AUTO_INCREMENT,
-    tipoDocCliente VARCHAR(10),
-    NroDocumentoCliente VARCHAR(20),
-    idSala INT,
-    NroAsiento INT,
-    idFuncion INT,
-    fechaEmision DATE,
+    NumeroEntrada INT AUTO_INCREMENT NOT NULL,
+    tipoDocCliente VARCHAR(10) NOT NULL,
+    NroDocumentoCliente CHAR(8) NOT NULL,
+    idSala INT NOT NULL,
+    NroAsiento INT NOT NULL,
+    idFuncion INT NOT NULL,
+    fechaEmision DATE NOT NULL,
     PRIMARY KEY (NumeroEntrada),
-    FOREIGN KEY (tipoDocCliente, NroDocumentoCliente) REFERENCES CLIENTE(tipoDoc, NroDocumento),
-    FOREIGN KEY (idSala, NroAsiento) REFERENCES ASIENTO(idSala, nroAsiento),
-    FOREIGN KEY (idFuncion) REFERENCES FUNCION(IdFuncion)
+    FOREIGN KEY (tipoDocCliente, NroDocumentoCliente) REFERENCES CLIENTE(tipoDoc, NroDocumento) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (idSala, NroAsiento) REFERENCES ASIENTO(idSala, nroAsiento) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (idFuncion) REFERENCES FUNCION(IdFuncion) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE PREFIERE (
-    tipoDocCliente VARCHAR(10),
-    NroDocumentoCliente VARCHAR(20),
-    idPublicidad INT,
-    Motivo VARCHAR(255),
+    tipoDocCliente VARCHAR(10) NOT NULL,
+    NroDocumentoCliente CHAR(8) NOT NULL,
+    idPublicidad INT NOT NULL,
+    Motivo VARCHAR(255) NOT NULL,
     PRIMARY KEY (tipoDocCliente, NroDocumentoCliente, idPublicidad),
-    FOREIGN KEY (tipoDocCliente, NroDocumentoCliente) REFERENCES CLIENTE(tipoDoc, NroDocumento),
-    FOREIGN KEY (idPublicidad) REFERENCES PUBLICIDAD(id)
+    FOREIGN KEY (tipoDocCliente, NroDocumentoCliente) REFERENCES CLIENTE(tipoDoc, NroDocumento) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (idPublicidad) REFERENCES PUBLICIDAD(id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
